0cde0e716bb2bee03faca938f3e152a8d412b38d
Oleh Novosad <7419136+neversad-dev@users.noreply.github.com>
2025-04-16 23:00:57 +0300

    Refactor file observer to use Flow for real-time updates
    Converted `getAllFiles` in `ImageRepository` to return a Flow for reactive file updates. Updated `InternalMemoryImageRepository` to emit events on file changes and adjusted `CollectionViewModel` to observe file changes using the new Flow mechanism.

--- composeApp/src/commonMain/kotlin/com/neversad/vidzerunok/core/domain/ImageRepository.kt
+++ composeApp/src/commonMain/kotlin/com/neversad/vidzerunok/core/domain/ImageRepository.kt
@@ -2,11 +2,12 @@ package com.neversad.vidzerunok.core.domain
 
 import com.neversad.vidzerunok.core.common.EmptyResult
 import com.neversad.vidzerunok.core.common.Error
+import kotlinx.coroutines.flow.Flow
 
 interface ImageRepository {
     suspend fun saveFile(path: String): EmptyResult<Error.Local>
 
-    suspend fun getAllFiles(): List<String>
+    fun getAllFiles(): Flow<List<String>>
 
     suspend fun deleteImage(file: String): EmptyResult<Error.Local>
 }
